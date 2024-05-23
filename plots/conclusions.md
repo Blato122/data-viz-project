79daf8d249b3435edea89dd2431bd4365483199d
blato <blator02@gmail.com>
2024-05-23 19:53:59 +0200

    added conclusions, moved read

---
+++ plots/conclusions.md
@@ -0,0 +1,39 @@
+**Gminy położone w górach:**
+- Szczawnica: Pieniny i Beskid Sądecki, samo centrum
+- Lipnica Wielka: Beskidy, niedaleko u podnóży Babiej Góry
+- Cisna: Bieszczady, centrum
+- Komańcza: Bieszczady, obrzeża
+- Krempna: Beskid Niski, przy samym Magurskim Parku Narodowym
+
+**Gminy położone przy granicy:**
+- Stubno: przy samej granicy z Ukrainą
+- Cedynia: przy samej granicy z Niemcami
+- Dołhobyczów: przy samej granicy z Ukrainą
+- Michałowo: niedaleko granicy z Białorusią i Puszczą Białowieską
+- Kodeń: przy samej granicy z Białorusią
+- Tuczna: przy samej granicy z Białorusią
+- Krynica Morska: przy samej granicy z Rosją i na Mierzei Wiślanej
+- Krynki: przy samej granicy z Białorusią
+- Trzcińsko-Zdrój: niedaleko granicy z Niemcami
+- Wilczęta: woj. warmińsko-mazurskie, okolice Elbląga, niedaleko granicy z Rosją
+
+**Pozostałe gminy:**
+- Baćkowice: niedaleko Świętokrzyskiego Parku Narodowego
+- Lelów: niedaleko Jury Krakowsko-Częstochowskiej
+- Czarnia: granica woj. mazowieckiego i woj. warmińsko-mazurskiego (granica województw)
+- Nur: granica woj. mazowieckiego i woj. podlaskiego (również granica województw)
+- Przedbórz: granica woj. świętokrzyskiego i woj. łódzkiego (i tutaj również)
+
+Widać zatem, że szczególnie utrudniony dostęp do szpitali mają gminy położone
+blisko granicy, szczególnie tej wschodniej. Może wynikać to z tego, że nie brałem
+pod uwagę zagranicznych szpitali. Nie zmienia to jednak faktu, że zwykle kierujemy
+się do szpitali w naszym państwie, a jeśli chodzi o szpitale polskie, to przy granicach 
+sytuacja jest gorsza.
+
+Dodatkowo, co raczej nie powinno dziwić, mieszkańcy gmin położonych w trudno dostępnych miejscach, 
+blisko natury (góry, Mierzeja Wiślana, Puszcza Białowieska, Świętokrzyski Park Narodowy), 
+również mogą mieć problem z szybkim dostaniem się do szpitala. Wpływać może na to niska gęstość
+zaludnienia i mniej rozwinięta infrastruktura drogowa.
+
+Jeśli chodzi o obszary ze szczególnie dobrym dostępem do szpitali, są to oczywiście 
+okolice dużych miast: konurbacja górnośląska, miasta wojewódzkie, wybrane miasta powiatowe.
--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,21 @@ with st.sidebar:
     selected = option_menu(
         menu_title="Menu", 
         options=["Mapa statyczna - gminy", 'Mapa interaktywna - gminy', "Statystyki - gminy", 
-                "Mapa statyczna - powiaty", 'Mapa interaktywna - powiaty', "Statystyki - powiaty"], 
-        icons=['house', 'gear', 'bi-bar-chart']*2, 
+                "Mapa statyczna - powiaty", 'Mapa interaktywna - powiaty', "Statystyki - powiaty",
+                "Wnioski"], 
+        icons=['house', 'gear', 'bi-bar-chart']*2 + ['bi-file-text'], 
         menu_icon="bi-menu-button", 
         default_index=0
     )
-interactive_mun = open('plots/interactive_mun.html')
-interactive_cnt = open('plots/interactive_cnt.html')
+
+with open('plots/conclusions.md', 'r') as conclusions_file, \
+     open('plots/interactive_mun.html', 'r') as interactive_mun_file, \
+     open('plots/interactive_cnt.html', 'r') as interactive_cnt_file:
+    
+    conclusions = conclusions_file.read()
+    interactive_mun = interactive_mun_file.read()
+    interactive_cnt = interactive_cnt_file.read()
+
 
 # st.image() instead of st.pyplot() now!
 if selected == "Mapa statyczna - gminy":
@@ -26,7 +34,7 @@ elif selected == "Mapa interaktywna - gminy":
     st.header("Interaktywna mapa najbliższych szpitali od centrum danej gminy", divider='rainbow')
     st.subheader("Najedź kursorem na wybraną gminę, żeby zobaczyć dokładne statystyki. Możesz również przybliżać mapę.")
     # st_folium('plots/interactive_mun.html', width=800, height=600)
-    components.html(interactive_mun.read(), width=800, height=600)
+    components.html(interactive_mun, width=800, height=600) # "The read() method returns the specified number of bytes from the file. Default is -1 which means the whole file.""
 elif selected == "Statystyki - gminy":
     st.header("Wybrane statystyki", divider='rainbow')
     # st.subheader("Kliknij przycisk w prawym górnym rogu, aby powiększyć mapę.")
@@ -40,8 +48,12 @@ elif selected == "Mapa interaktywna - powiaty":
     st.header("Interaktywna mapa najbliższych szpitali od centrum danego powiatu", divider='rainbow')
     st.subheader("Najedź kursorem na wybrany powiat, żeby zobaczyć dokładne statystyki. Możesz również przybliżać mapę.")
     # st_folium('plots/interactive_mun.html', width=800, height=600)
-    components.html(interactive_cnt.read(), width=800, height=600)
+    components.html(interactive_cnt, width=800, height=600)
 elif selected == "Statystyki - powiaty":
     st.header("Wybrane statystyki", divider='rainbow')
     st.subheader("Mniej ciekawe, niż w przypadku gmin.")
     st.image("plots/statistics_cnt.png")
+
+elif selected == "Wnioski":
+    st.header("Wnioski z przedstawionych danych", divider='rainbow')
+    st.markdown(conclusions)
